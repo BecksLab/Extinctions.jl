194549b879ae89ced9dbaa5f671586ab5ddd4843
Tanya Strydom <tanya.strydom@icloud.com>
2025-02-19 16:06:06 +0200

    〽 random extinction protect

--- src/lib/extinction.jl
+++ src/lib/extinction.jl
@@ -5,14 +5,37 @@ extinction(N::SpeciesInteractionNetwork, end_richness::Int64)
     the richness is less than or equal to that specified by `end_richness`.
 """
 function extinction(
-    N::SpeciesInteractionNetwork{<:Partiteness,<:Binary},
-    end_richness::Int64,
+    N::SpeciesInteractionNetwork{<:Partiteness,<:Binary};
+    end_richness::Int64 = 0,
+    protect::Symbol = :basal,
 )
     if richness(N) <= end_richness
         throw(ArgumentError("Richness of starting community is less than final community"))
     end
+    if protect ∉ [:none, :basal, :consumer]
+        error("Invalid value for protect -- must be :none, :basal or :consumer")
+    end
 
+    # get list for all species
     extinction_list = StatsBase.shuffle(SpeciesInteractionNetworks.species(N))
+
+    # get generality of all spp
+    gen = SpeciesInteractionNetworks.generality(N)
+
+    # we must protect basal species - remove them from extinction list
+    if protect == :basal
+        # identify basal species
+        filter!(v -> last(v) == 0, gen)
+        # remove the species previously identified as basal
+        filter!(x -> x ∉ collect(keys(gen)), extinction_list)
+    elseif protect == :consumer
+        # identify basal species
+        filter!(v -> last(v) > 0, gen)
+        # remove the species previously identified as basal
+        filter!(x -> x ∉ collect(keys(gen)), extinction_list)
+    end
+
+    # create object to store networks at each timestamp
     network_series = Vector{SpeciesInteractionNetwork{<:Partiteness,<:Binary}}()
     # push initial network
     push!(network_series, deepcopy(N))
@@ -36,7 +59,6 @@ function extinction(
     if richness(N) <= end_richness
         throw(ArgumentError("Richness of final community is less than starting community"))
     end
-    #check extinction method
     if protect ∉ [:none, :basal, :consumer]
         error("Invalid value for protect -- must be :none, :basal or :consumer")
     end
