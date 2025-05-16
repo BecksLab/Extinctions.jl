e846added1b0b452ac519509ed37df470a7bba32
Tanya Strydom <tanya.strydom@icloud.com>
2025-05-16 12:17:18 +0100

    ✨ network test case for tests

---
+++ test/lib/build_network.jl
@@ -0,0 +1,17 @@
+using SpeciesInteractionNetworks
+
+spp = [:wolf, :fox, :hawk, :vole, :rat, :plant]
+nodes = Unipartite(spp)
+
+int_matrix = Bool[
+    0 1 1 0 0 0
+    0 0 0 1 1 0
+    0 0 0 0 1 0
+    0 0 0 0 0 0
+    0 0 0 0 0 1
+    0 0 0 0 0 0
+]
+edges = Binary(int_matrix)
+
+network = SpeciesInteractionNetwork(nodes, edges)
+N = network
