13208cbb3a290e9e806f53d0e1e47e0d76683090
Tanya Strydom <tanya.strydom@icloud.com>
2025-07-18 20:22:25 +0100

    🐛 on R50 calculations

--- src/lib/robustness.jl
+++ src/lib/robustness.jl
@@ -13,6 +13,9 @@ function robustness(Ns::Vector{T}; threshold::Int = 50) where {T<:SpeciesInterac
     # get first index in network series that is equal to or less than e50
     net_in = findfirst(x -> x <= thresh_rich, richness.(Ns))
 
+    # number of primary extinctions
+    primary = net_in - 1
+
     # return prop of primary extinctions as total number primary extinctions (length Ns - 1)
-    return net_in / (length(Ns) - 1)
+    return primary/init_rich
 end
